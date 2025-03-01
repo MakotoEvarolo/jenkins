4ed66a7cff626aacb2abc49692a4dc5943ac7e37
MakotoEvarolo <makmak.evarolo@gmail.com>
2025-03-01 16:09:26 +0800

    change

--- test_calculator.py
+++ test_calculator.py
@@ -6,10 +6,10 @@ class TestCalculator(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(2, 4), 5)
 
     def test_subtract(self):
-        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(5, 4), 2)
 
     def test_multiply(self):
         self.assertEqual(self.calc.multiply(4, 3), 12)
